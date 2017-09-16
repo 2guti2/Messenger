11f56f9c261cda6891e1a7d65e0139de296209b8
Bruno Vezoli <brunvezoli@hotmail.com>
2017-09-16 17:39:02 -0300

    Start Protocol

---
+++ Messenger/Business/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    internal class Server
+    {
+        public IList<Client> ConnectedClients { get; set; }
+
+        public Server()
+        {
+            ConnectedClients = new List<Client>();
+        }
+
+        public void ConnectClient(Client client)
+        {
+            ConnectedClients.Add(client);
+        }
+    }
+}
