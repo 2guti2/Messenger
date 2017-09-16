743d6d1e61366bd8e5f1dd26334a9c09d09b345f
Bruno <brunvezoli@hotmail.com>
2017-09-16 17:37:49 -0300

    Start Protocol

--- Messenger/Server/Program.cs
+++ Messenger/Server/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business;
+using Persistence;
+using Protocol;
 
 namespace Server
 {
@@ -10,6 +13,14 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            var store = new Store();
+            var bussinessController = new BussinessController(store);
+            var server = new ServerProtocol();
+            server.Start("127.0.0.1", 6500);
+            while (true)
+            {
+                server.AcceptRequest();
+            }
         }
     }
 }
