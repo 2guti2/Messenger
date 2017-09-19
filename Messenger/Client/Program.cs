5f78dcd4a76da413d7ad6fa6a98923428ab1e277
Bruno Vezoli <brunvezoli@hotmail.com>
2017-09-19 18:30:45 -0300

    Add Connection class

--- Messenger/Client/Program.cs
+++ Messenger/Client/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using Business;
-using Protocol;
+﻿using Protocol;
 
 namespace Client
 {
@@ -14,9 +6,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            var clientRequestManager = new ClientProtocol();
-            var clientController = new ClientController(clientRequestManager);
-            clientController.Init();
+       
         }
     }
 }
